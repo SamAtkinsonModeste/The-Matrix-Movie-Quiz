a32a10bd0975db04a5e3b5c33624c202a2270a72
Samantha Atkinson <sammodeste@yahoo.com>
2024-08-10 01:52:27 +0100

    corrected format of objects nested in levelOne Arrray

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,39 +1,34 @@
 const levelOne = [
     {
-        pic: "assets/images/agent_smith.jpg",
+        pic: `<img src="assets/images/agent_smith.jpg">`,
        answers: [
-        {
-         options: `<li><label for="cypher"> Cypher <input type="radio" id="cypher" name="character" value="Cypher"></label></li>`, 
-         options: `<li><label for="agent-smith" class="correct"> Agent Smith <input type="radio" id="agent-smith" name="character" value="Agent-smith"></label></li>` , 
-         options: `<li><label for="neo" class="correct"> Neo <input type="radio" id="neo" name="character" value="Neo"></label></li>`,
-         options: `<li><label for="apoc" class="correct"> Apoc <input type="radio" id="apoc" name="character" value="Apoc"></label></li>`,
-         options: `<li><label for="anderson"> Mr Anderson <input type="radio" id="anderson" name="character" value="Mr Anderson"></label></li>`,    
-        },
+        {options: `<li><label for="cypher"> Cypher <input type="radio" id="cypher" name="character" value="Cypher"></label></li>`}, 
+         {options: `<li><label for="agent-smith" class="correct"> Agent Smith <input type="radio" id="agent-smith" name="character" value="Agent-smith"></label></li>`} , 
+         {options: `<li><label for="neo" class="correct"> Neo <input type="radio" id="neo" name="character" value="Neo"></label></li>`},
+         {options: `<li><label for="apoc" class="correct"> Apoc <input type="radio" id="apoc" name="character" value="Apoc"></label></li>`},
+         {options: `<li><label for="anderson"> Mr Anderson <input type="radio" id="anderson" name="character" value="Mr Anderson"></label></li>`}   
+        
         ]
     },
     {
-        pic: "assets/images/apoc.webp",
+        pic:`<img src="assets/images/apoc.webp">`,
         answers: [
-            {
-             options: `<li><label for="tank"> Tank <input type="radio" id="tank" name="character" value="Tank"></label></li>`, 
-             options: `<li><label for="white-rabbit"> White Rabbit <input type="radio" id="white-rabbit" name="character" value="White-Rabbit"></label></li>` , 
-             options: `<li><label for="mouse"> Mouse <input type="radio" id="mouse" name="character" value="Mouse"></label></li>`,
-             options: `<li><label for="switch"> Switch <input type="radio" id="switch" name="character" value="Switch"></label></li>`, 
-             options: `<li><label for="apoc" class="correct"> Apoc <input type="radio" id="apoc" name="character" value="Apoc"></label></li>`,   
-            },
+            {options: `<li><label for="tank"> Tank <input type="radio" id="tank" name="character" value="Tank"></label></li>`}, 
+             {options: `<li><label for="white-rabbit"> White Rabbit <input type="radio" id="white-rabbit" name="character" value="White-Rabbit"></label></li>`} , 
+             {options: `<li><label for="mouse"> Mouse <input type="radio" id="mouse" name="character" value="Mouse"></label></li>`},
+             {options: `<li><label for="switch"> Switch <input type="radio" id="switch" name="character" value="Switch"></label></li>`}, 
+             {options: `<li><label for="apoc" class="correct"> Apoc <input type="radio" id="apoc" name="character" value="Apoc"></label></li>`},   
+        
             ]
     },
     {
-        pic: "assets/images/Trinity.webp",
+        pic: `<img src="assets/images/Trinity.webp">`,
         answers: [
-            {
-             options: `<li><label for="dozer"> Tank <input type="radio" id="dozer" name="character" value="Tank"></label></li>`,
-             options: `<li><label for="oracle"> Oracle <input type="radio" id="oracle" name="character" value="Oracle"></label></li>`,  
-             options: `<li><label for="white-rabbit"> White Rabbit <input type="radio" id="white-rabbit" name="character" value="White-Rabbit"></label></li>` , 
-             options: `<li><label for="trinity" class="correct"> Trinity <input type="radio" id="trinity" name="character" value="Trinity"></label></li>`, 
-             options: `<li><label for="mouse"> Mouse <input type="radio" id="mouse" name="character" value="Mouse"></label></li>`,
-              
-            },
+            {options: `<li><label for="dozer"> Dozer <input type="radio" id="dozer" name="character" value="Tank"></label></li>`},
+             {options: `<li><label for="oracle"> Oracle <input type="radio" id="oracle" name="character" value="Oracle"></label></li>`},  
+             {options: `<li><label for="white-rabbit"> White Rabbit <input type="radio" id="white-rabbit" name="character" value="White-Rabbit"></label></li>`} , 
+             {options: `<li><label for="trinity" class="correct"> Trinity <input type="radio" id="trinity" name="character" value="Trinity"></label></li>`}, 
+             {options: `<li><label for="mouse"> Mouse <input type="radio" id="mouse" name="character" value="Mouse"></label></li>`}
             ]
     }
 ];
