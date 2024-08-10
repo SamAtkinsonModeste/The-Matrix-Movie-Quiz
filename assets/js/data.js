8d3a6a2da3d9d7d990ce6e9be4cd8938e1b5a61d
Samantha Atkinson <sammodeste@yahoo.com>
2024-08-10 03:02:55 +0100

    js: file:data - moved class attribute to input elements

--- assets/js/data.js
+++ assets/js/data.js
@@ -3,9 +3,9 @@ const levelOne = [
         pic: `<img src="assets/images/agent_smith.jpg">`,
        answers: [
         {options: `<li><label for="cypher"> Cypher <input type="radio" id="cypher" name="character" value="Cypher"></label></li>`}, 
-         {options: `<li><label for="agent-smith" class="correct"> Agent Smith <input type="radio" id="agent-smith" name="character" value="Agent-smith"></label></li>`} , 
-         {options: `<li><label for="neo" class="correct"> Neo <input type="radio" id="neo" name="character" value="Neo"></label></li>`},
-         {options: `<li><label for="apoc" class="correct"> Apoc <input type="radio" id="apoc" name="character" value="Apoc"></label></li>`},
+         {options: `<li><label for="agent-smith"> Agent Smith <input class="correct" type="radio" id="agent-smith" name="character" value="Agent-smith"></label></li>`} , 
+         {options: `<li><label for="neo"> Neo <input type="radio" id="neo" name="character" value="Neo"></label></li>`},
+         {options: `<li><label for="apoc"> Apoc <input type="radio" id="apoc" name="character" value="Apoc"></label></li>`},
          {options: `<li><label for="anderson"> Mr Anderson <input type="radio" id="anderson" name="character" value="Mr Anderson"></label></li>`}   
         
         ]
@@ -17,7 +17,7 @@ const levelOne = [
              {options: `<li><label for="white-rabbit"> White Rabbit <input type="radio" id="white-rabbit" name="character" value="White-Rabbit"></label></li>`} , 
              {options: `<li><label for="mouse"> Mouse <input type="radio" id="mouse" name="character" value="Mouse"></label></li>`},
              {options: `<li><label for="switch"> Switch <input type="radio" id="switch" name="character" value="Switch"></label></li>`}, 
-             {options: `<li><label for="apoc" class="correct"> Apoc <input type="radio" id="apoc" name="character" value="Apoc"></label></li>`},   
+             {options: `<li><label for="apoc"> Apoc <input class="correct" type="radio" id="apoc" name="character" value="Apoc"></label></li>`},   
         
             ]
     },
@@ -27,7 +27,7 @@ const levelOne = [
             {options: `<li><label for="dozer"> Dozer <input type="radio" id="dozer" name="character" value="Tank"></label></li>`},
              {options: `<li><label for="oracle"> Oracle <input type="radio" id="oracle" name="character" value="Oracle"></label></li>`},  
              {options: `<li><label for="white-rabbit"> White Rabbit <input type="radio" id="white-rabbit" name="character" value="White-Rabbit"></label></li>`} , 
-             {options: `<li><label for="trinity" class="correct"> Trinity <input type="radio" id="trinity" name="character" value="Trinity"></label></li>`}, 
+             {options: `<li><label for="trinity"> Trinity <input  class="correct" type="radio" id="trinity" name="character" value="Trinity"></label></li>`}, 
              {options: `<li><label for="mouse"> Mouse <input type="radio" id="mouse" name="character" value="Mouse"></label></li>`}
             ]
     }
