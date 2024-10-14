70453168df49c1d0bdfc8e41a556708d1b602eb9
Samantha Atkinson <sammodeste@yahoo.com>
2024-10-14 16:30:44 +0100

    js: player can answer all quesitons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,16 @@
+/**
+ * Function to get elements by thier Id
+ * 
+ */
+const elementId = el => document.getElementById(el);
+
+/**Global Variables */
 let randomCharacter, currentQuestionIndex;
+const optionsContainer = elementId( 'answer-options' );
+const imagesOfusersQuestionsCorrect = [];
+const imagesOfusersQuestionsWrong = [];
 let currentScore = 0;
 let finalScore = 0;
-const optionsContainer = document.getElementById( 'answer-options' );
-const usersScore = document.getElementById( 'users-score' );
-const overlayHowToPlay = document.getElementById( 'overlay-how-to-play' );
 
 
 /**
@@ -13,26 +20,26 @@ const overlayHowToPlay = document.getElementById( 'overlay-how-to-play' );
  * function gameEntryButons gives the user control of the game start and info.
  */
 document.addEventListener( "DOMContentLoaded", () => {
-	let begin = document.getElementById( 'begin' );
+	let begin = elementId('begin');
+	
 	begin.addEventListener( 'click', () => {
-		let overlayHome = document.getElementById( 'overlay-home' );
+		let overlayHome = elementId( 'overlay-home' );
 		overlayHome.classList.add( 'hide' );
-		document.getElementById( 'entry-controls-container' ).classList.remove( 'hide' );
-		document.getElementById( 'player-name' ).focus();
+		elementId( 'entry-controls-container' ).classList.remove( 'hide' );
+		elementId( 'player-name' ).focus();
 	} );
-	//NOTE: gameEntry Function called
+
 	gameEntryButtons();
-	document.getElementById( 'start-game' ).addEventListener( 'click', startQuiz );
-	document.getElementById( 'nextBtn' ).addEventListener( 'click', function() {
-		currentQuestionIndex++;
+	elementId('start-game').addEventListener('click',startQuiz);
+	elementId('nextBtn').addEventListener('click', function(){
+		currentQuestionIndex++
 		setNextCharacter();
-	} );
-	document.getElementById( 'start-again' ).addEventListener( 'click', playAgainOption );
-	document.getElementById( 'landHomeBtn' ).addEventListener( 'click', resetGameToEntry );
-	document.getElementById( 'homeBtn' ).addEventListener( 'click', resetGame );
-	document.getElementById( 'submitBtn' ).addEventListener( 'click', usersResults );
-	document.getElementById( 'close-users-score' ).addEventListener( 'click', closeScoreResults );
-} );
+	});
+	
+	
+});
+
+
 
 
 /**
@@ -46,13 +53,15 @@ function gameEntryButtons() {
 	for ( let btn of btns ) {
 		btn.addEventListener( 'click', function() {
 			if ( this.getAttribute( 'id' ) === 'howToPlay' ) {
-				overlayHowToPlay.classList.remove( 'hide' );
+				elementId('overlay-how-to-play').classList.remove( 'hide' );
 			} else if ( this.getAttribute( 'id' ) === 'close-HowTo-btn' ) {
-				overlayHowToPlay.classList.add( 'hide' );
+				elementId('overlay-how-to-play').classList.add( 'hide' );
 			} else if ( this.getAttribute( 'id' ) === 'submitBtn' ) {
-				document.getElementById( 'entry-controls-container' ).classList.add( 'hide' );
-				document.getElementById( 'game-container' ).classList.add( 'hide' );
-			}
+				elementId( 'entry-controls-container' ).classList.add( 'hide' );
+				elementId( 'game-container' ).classList.add( 'hide' );
+			} 
+			  
+			
 		} );
 	}
 }
@@ -65,41 +74,50 @@ function gameEntryButtons() {
  */
 function randomArray( arr ) {
 	//below resuffles the order of the arrays items
-	const randomArrItem = arr.sort( () => Math.random() - .5 );
+	const randomArrItem = arr.sort( () => Math.random() - 0.5 );
 	return randomArrItem;
 }
+
+
 
 
 /**
  * function start quiz
  * Checks user's input has a value.
- * setNextCharacter function is called.
+ * 
  */
-function startQuiz( level ) {
-	const playerName = document.getElementById( 'player-name' );
+function startQuiz(){
+	console.log("I start the game");
+	const playerName = elementId( 'player-name' );
+	console.log(playerName)
 	if ( playerName.value === "" ) {
-		alert( 'You need to enter your name first!' );
+		elementId('alert-name').classList.remove('hide');
+		elementId("start-game").disabled = true;
+		const alertBoxClose = elementId('close-alert-btn');
+		alertBoxClose.addEventListener('click', ()=>{ 
+			elementId('alert-name').classList.add('hide');
+			elementId("start-game").disabled = false;
+		});
+
 	} else {
 		if ( playerName.value !== "" ) {
 			document.getElementById( 'game-container' ).classList.remove( 'hide' );
 			document.getElementById( 'entry-controls-container' ).classList.add( 'hide' );
 		}
 	}
-	level = matrixCharacters;
-	randomCharacter = randomArray( level );
-	currentQuestionIndex = 0;
-	setNextCharacter();
+  randomCharacter = randomArray(matrixCharacters);
+  currentQuestionIndex = 0;
+  setNextCharacter();
 }
 
 
-/**
- * setNextCharacter Function
- * Calls displayCharacterOptions function
- * and passes in the array and it's current index as the argument
- */
-function setNextCharacter() {
-	resetButton();
-	displayCharacterOptions( randomCharacter[ currentQuestionIndex ] );
+
+
+
+function setNextCharacter(){
+  
+ displayCharacterOptions(randomCharacter[currentQuestionIndex]);
+  
 }
 
 
@@ -110,16 +128,18 @@ function setNextCharacter() {
  * 2. Options/answers to who the character is.
  */
 function displayCharacterOptions( option ) {
-	let ulHTML = `<ul>`;
+	let ulHTML = `<ul id="radio-options">`;
 	option.answers.forEach( answer => {
 		let li = answer.options;
 		ulHTML += `${li}`;
 	} );
 	ulHTML += `</ul>`;
-	document.getElementById( 'character-image' ).innerHTML = option.pic;
-	document.getElementById( 'answer-options' ).innerHTML = ulHTML;
+	elementId( 'character-image' ).innerHTML = option.pic;
+	elementId( 'answer-options' ).innerHTML = ulHTML;
 	optionsContainer.addEventListener( 'click', catchAnswers );
+	
 }
+
 
 
 /**
@@ -128,10 +148,6 @@ function displayCharacterOptions( option ) {
  * seperates correct character images
  * seperates wrong character images
  */
-//NOTE: Global Arrays
-let imagesOfusersQuestionsCorrect = [];
-let imagesOfusersQuestionsWrong = [];
-
 function catchAnswers( evt ) {
 	const selectedBtn = evt.target;
 	const nextBtn = document.getElementById( 'nextBtn' );
@@ -161,135 +177,6 @@ function catchAnswers( evt ) {
 }
 
 
-/**
- * toggles nextBtn hide class on
- */
-function resetButton() {
-	const nextBtn = document.getElementById( 'nextBtn' );
-	nextBtn.classList.add( 'hide' );
-}
+console.log(imagesOfusersQuestionsCorrect);
+console.log(imagesOfusersQuestionsWrong);
 
-
-/**
- * display users results.
- * if it's over 180 score they get a diffirent congrats message.
- * if under 180 different congrats message.
- * all players gets 2 ul displayed.
- * 1 ul for images of correct characters.
- * 1 ul for the wrong characters.
- */
-function usersResults() {
-	if ( finalScore > 180 ) {
-		document.getElementById( 'high-scorer' ).classList.remove( 'hide' );
-		document.getElementById( 'users-score' ).classList.remove( 'hide' );
-		document.getElementById( 'high-score-user' ).innerHTML = finalScore;
-		if ( imagesOfusersQuestionsCorrect.length !== 0 ) {
-			let correctHighLiHTML = ``;
-			for ( let i of imagesOfusersQuestionsCorrect ) {
-				correctHighLiHTML += `<li>${i}</li>`;
-			}
-			document.getElementById( 'charactersKnownHigh' ).classList.remove( 'hide' );
-			document.getElementById( 'knownUlHigh' ).innerHTML = correctHighLiHTML;
-		}
-		if ( imagesOfusersQuestionsWrong.length !== 0 ) {
-			let wrongHighLiHTML = ``;
-			for ( let i of imagesOfusersQuestionsWrong ) {
-				wrongHighLiHTML += `<li>${i}</li>`;
-			}
-			document.getElementById( 'charactersUnknownHigh' ).classList.remove( 'hide' );
-			document.getElementById( 'unknownUlHigh' ).innerHTML = wrongHighLiHTML;
-		}
-	}
-	if ( finalScore < 180 ) {
-		document.getElementById( 'low-score' ).classList.remove( 'hide' );
-		document.getElementById( 'users-score' ).classList.remove( 'hide' );
-		document.getElementById( 'low-score-user' ).innerHTML = finalScore;
-		if ( imagesOfusersQuestionsCorrect.length !== 0 ) {
-			let lowCorrectLiHTML = ``;
-			for ( let i of imagesOfusersQuestionsCorrect ) {
-				lowCorrectLiHTML += `<li>${i}</li>`;
-				
-			}
-			document.getElementById( 'charactersKnownLow' ).classList.remove( 'hide' );
-			document.getElementById( 'knownUlLow' ).innerHTML = lowCorrectLiHTML;
-		}
-		if ( imagesOfusersQuestionsWrong.length !== 0 ) {
-			let wrongLowLiHTML = ``;
-			for ( let i of imagesOfusersQuestionsWrong ) {
-				wrongLowLiHTML += `<li>${i}</li>`;
-				
-			}
-			document.getElementById( 'charactersUnknownLow' ).classList.remove( 'hide' );
-			document.getElementById( 'unknownUlLow' ).innerHTML = wrongLowLiHTML;
-		}
-		document.getElementById( 'entry-controls-container' ).classList.add( 'hide' );
-		document.getElementById( 'game-container' ).classList.add( 'hide' );
-	}
-}
-
-
-/**
- * complete reset game
- * resets everything so player name can change
- */
-function resetGame() {
-	document.getElementById( 'player-name' ).value = "";
-	document.getElementById( 'player-name' ).focus();
-	const radioBtns = document.querySelectorAll( 'input[name="character"]' );
-	for ( let btn of radioBtns ) {
-		if ( btn.checked === true ) {
-			btn.checked = false;
-		}
-	}
-	document.getElementById( 'submitBtn' ).classList.add( 'hide' );
-	currentScore = 0;
-	currentQuestionIndex = 0;
-	document.getElementById( 'game-container' ).classList.add( 'hide' );
-	document.getElementById( 'entry-controls-container' ).classList.remove( 'hide' );
-	document.getElementById( 'overlayPlayAgain' ).classList.add( 'hide' );
-	imagesOfusersQuestionsCorrect = [];
-	imagesOfusersQuestionsWrong = [];
-}
-
-
-/**
- * reset game
- * rest sets back to the entry options.
- * which is how to play and players input.
- * will still have previous players name in the input
- */
-function resetGameToEntry() {
-	const radioBtns = document.querySelectorAll( 'input[name="character"]' );
-	for ( let btn of radioBtns ) {
-		if ( btn.checked === true ) {
-			btn.checked = false;
-		}
-	}
-	document.getElementById( 'submitBtn' ).classList.add( 'hide' );
-	currentScore = 0;
-	currentQuestionIndex = 0;
-	document.getElementById( 'game-container' ).classList.add( 'hide' );
-	document.getElementById( 'entry-controls-container' ).classList.remove( 'hide' );
-	document.getElementById( 'overlayPlayAgain' ).classList.add( 'hide' );
-	imagesOfusersQuestionsCorrect = [];
-	imagesOfusersQuestionsWrong = [];
-}
-
-
-/**
- * closes the users score results
- */
-function closeScoreResults() {
-	usersScore.classList.add( 'hide' );
-	document.getElementById( 'overlayPlayAgain' ).classList.remove( 'hide' );
-}
-
-
-/**
- * options to play again
- * option play again
- * or go back to entry point with everything reset
- */
-function playAgainOption() {
-	resetGame();
-}
